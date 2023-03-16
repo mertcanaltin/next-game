89e06c69f83a1178ce2ac90da4489b81e38ab0e4
Mert Can Altın <mertgold60@gmail.com>
2023-03-16 22:25:11 +0300

    chracter movement

--- engine/tools/Canvas/index.tsx
+++ engine/tools/Canvas/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react';
 
 type Props = {
   map: string,
@@ -9,62 +9,82 @@ type Props = {
 }
 
 const Canvas = (props: Props) => {
+  const { map, player, ...rest } = props;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [position, setPosition] = useState({ x: 0, y: 4 });
 
-  const { map, player, ...rest } = props
-
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+  console.log(position)
 
   useEffect(() => {
-    const canvas = canvasRef.current
-    const context = canvas?.getContext('2d')
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext('2d');
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case 'w':
+          setPosition(prevPos => ({ ...prevPos, y: prevPos.y - 10 }));
+          break;
+        case 'a':
+          setPosition(prevPos => ({ ...prevPos, x: prevPos.x - 10 }));
+          break;
+        case 's':
+          setPosition(prevPos => ({ ...prevPos, y: prevPos.y + 10 }));
+          break;
+        case 'd':
+          setPosition(prevPos => ({ ...prevPos, x: prevPos.x + 10 }));
+          break;
+        default:
+          break;
+      }
+    };
 
     const canvasRender = (ctx: CanvasRenderingContext2D) => { 
-      requestAnimationFrame(() => drawCanvas(context!));
+      requestAnimationFrame(() => drawCanvas(ctx));
 
       const handleResize = () => {
         ctx.canvas.height = window.innerHeight;
         ctx.canvas.width = window.innerWidth;
       };
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
+      handleResize();
+      window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
-     
-    }
+      return () => window.removeEventListener("resize", handleResize);
+    };
 
     const drawCanvas = (ctx:CanvasRenderingContext2D) => {
       const canvas = ctx.canvas;
       const mapImage = new Image();
-
       mapImage.src = map;
 
       let scaleFactor = Math.max(canvas?.width! / mapImage.width, canvas?.height! / mapImage.height);
       let newWidth = mapImage.width * scaleFactor;
       let newHeight = mapImage.height * scaleFactor;
-
       let x = (canvas?.width! / 2) - (newWidth / 2);
       let y = (canvas?.height! / 2) - (newHeight / 2);
 
-      const chracterImg = new Image();
-  
-      chracterImg.src = player.chracterImage;
-
+      const characterImg = new Image();
+      characterImg.src = player.chracterImage;
 
       ctx.drawImage(mapImage, x, y, newWidth, newHeight);
-      ctx.drawImage(chracterImg, 0,4,80,80);
-    
+      ctx.drawImage(characterImg, position.x, position.y, 80, 80);
+
       requestAnimationFrame(() => drawCanvas(ctx));
-    }
+    };
 
     const render = () => {
-      canvasRender(context!)
-    }
+      canvasRender(context!);
+      window.addEventListener('keydown', handleKeyDown);
+    };
 
     render();
-  }, [])
 
-  return <canvas ref={canvasRef} {...rest} />
-}
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [map, player]);
 
-export default Canvas
+  return <canvas ref={canvasRef} {...rest} />;
+};
+
+export default Canvas;
